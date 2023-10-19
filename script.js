edf419de70f8109597caf3bd7b96d9b2ad8b0156
John <johnsief12@gmail.com>
2023-10-19 17:57:36 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let name =
+{
+	username: "john",
+	password: "guessmypassword"
+}
+
+let database = [
+	{
+		username: "john",
+		password: "guessmypassword"
+	}
+];
+
+let newsFeed = [
+	{
+		username: "Bobby",
+		timeline: "So tired from all that learning!"
+	},
+	{
+		username: "Sally",
+		timeline: "Javascript is sooooo cool!"
+	},
+	{
+		username: "Mitch",
+		timeline: "Javascript is pretty cool!"
+	}
+];
